c7353b3850c1293f70cd0797e484faaa646e53a8
Matilda Lerner <lernertilda@gmail.com>
2022-11-08 09:09:14 -0800

    start

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("==============================="); 
+        System.out.println("Rank-Choice Vote Ballot Counter");
+        System.out.println("By Riley and Tilda :)");
+        System.out.println("===============================");
     }
 }
